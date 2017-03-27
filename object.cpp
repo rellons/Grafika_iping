81ee795fe9348b07b6ed93563638e87985bc872d
yeksa <yeksa.ningrat@gmail.com>
2017-03-27 15:29:49 +0700

    added driver on object.cpp

--- object.cpp
+++ object.cpp
@@ -1,4 +1,5 @@
 #include "point.cpp"
+#include "Line.h"
 
 using namespace std;
 
@@ -7,6 +8,7 @@ class Object{
       list<Point> Points;
       bool hide;
       string Name;
+      Line l;
 
   public :
     Object(){
@@ -46,14 +48,35 @@ class Object{
     }
 
     void drawObject(){
-      list<Point>::iterator it;
-      for (it=Points.begin(); it != Points.end(); ++it){
+      list<Point>::iterator it,it2;
+      it = Points.begin();
+      it2 = ++(Points.begin());
+      for (it,it2; it != Points.end(); ++it,++it2){
           list<Point>::iterator Point1 = it;
-          list<Point>::iterator Point2 = it++;
+          list<Point>::iterator Point2 = it2;
           if(Point2 == Points.end()){
-            Point2 = Points.begin()++;
+            Point2 = Points.begin();
           }
-        //  DrawLine(it,it++)
+          l.insertLine((*Point1).getX(), (*Point1).getY(), (*Point2).getX(), (*Point2).getY(), 255, 255 , 255);
       }
     }
 };
+
+int main() {
+  Point p1 (100, 200);
+  Point p2 (100, 300);
+  Point p3 (200, 300);
+  Point p4 (200, 200);
+  system("clear");
+  Object o ("object1");
+  cout << o.getName() << endl;
+
+  o.addPoint(p1);
+  o.addPoint(p2);
+  o.addPoint(p3);
+  o.addPoint(p4);
+
+  o.drawObject();
+
+  return 0;
+}
